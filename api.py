122e8684fcc23f07f81750790cfcdf26861b9fb3
MatheusLeall <matheuscardoso.pro@gmail.com>
2023-10-31 17:37:25 -0300

    feat: add cors configuration

--- api.py
+++ api.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 from routes.player import router
+from fastapi.middleware.cors import CORSMiddleware
+
+CORS = [
+    "http://localhost:3000",
+    "http://localhost:3002"
+]
 
 app = FastAPI()
 
 app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
